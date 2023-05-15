921385f3f6c1941299c5feb33c7d36c9d61a7131
Alejandro Moran <alexmoranduque@gmail.com>
2023-05-14 19:36:40 -0600

    Initial commit

---
+++ Controllers/RobotControlador.py
@@ -0,0 +1,37 @@
+import pygame
+from RobotModelo import RobotModelo
+class RobotControlador:
+
+ def actualizar(self,delta_tiempo,modelo):
+ # Actualiza el reloj del modelo
+ modelo.temporizador-=delta_tiempo
+ # Si el tiempo del reloj se acaba, cambia el frame y reinicia
+el reloj
+ if modelo.temporizador<0:
+
+modelo.temporizador+=modelo.tiempo_reiniciador_temporizador
+ modelo.frame+=1
+ modelo.frame%=2
+
+ # Obtiene el estado actual de las teclas presionadas
+ teclas=pygame.key.get_pressed()
+
+ # Mueve el modelo según las teclas presionadas
+ if teclas[pygame.K_RIGHT]:
+ modelo.x += modelo.pixelporseg*(delta_tiempo/1000)
+ elif teclas[pygame.K_LEFT]:
+ modelo.x -= modelo.pixelporseg*(delta_tiempo/1000)
+ elif teclas[pygame.K_UP]:
+ modelo.y -= modelo.pixelporseg*(delta_tiempo/1000)
+ elif teclas[pygame.K_DOWN]:
+ modelo.y += modelo.pixelporseg*(delta_tiempo/1000)
+
+ # Limita el movimiento del modelo dentro de la pantalla
+ if modelo.x<=0:
+ modelo.x=0
+ if modelo.y<=0:
+ modelo.y=0
+ if modelo.x>=640-32:
+ modelo.x=640-32
+ if modelo.y>=480-32:
+ modelo.y=480-32
---
+++ Views/RobotVista.py
@@ -0,0 +1,12 @@
+import pygame
+from RobotModelo import RobotModelo # importa la clase RobotModelo
+desde el archivo RobotModelo.py
+class RobotVista:
+    # constructor, recibe una ruta de imagen como argumento
+    def __init__(self, ruta_imagen):
+        self.imagen = pygame.image.load(ruta_imagen) # carga la imagen en memoria
+
+    # método para dibujar la imagen en la pantalla, recibe la pantalla y el modelo como argumentos
+    def dibujar(self, pantalla, modelo):
+        area = pygame.Rect(modelo.frame * 32, 0, 32, 32) # crea un rectángulo para definir el área de la imagen a dibujar, basado en el frame actual del modelo
+        pantalla.blit(self.imagen, (modelo.x, modelo.y), area) #dibuja la imagen en la pantalla en la posición del modelo y la región definida por el rectángulo "area"
